4b152fffd8f68f2f3f1cbb30d74ff2f75da78854
CJ Pallari <cjpallari@gmail.com>
2025-03-01 01:31:20 -0800

    added sentiment analysis score for each title

--- historical.py
+++ historical.py
@@ -5,11 +5,12 @@ import pprint
 import praw
 from datetime import datetime, timezone
 import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 reddit = praw.Reddit(
-    client_id=CLIENT_ID,  # Found at the top of your app settings
-    client_secret=CLIENT_SECRET,  # The secret you see in the image
-    user_agent="python:ticker_skimmer:v1.0",  # A short description (e.g., "TickerSkimmer bot")
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    user_agent="python:ticker_skimmer:v1.0",
     read_only=True
 )
 
@@ -28,15 +29,16 @@ def get_historic_data(symbol):
 
 
 def get_posts():
+    analyzer = SentimentIntensityAnalyzer()
     subreddit = reddit.subreddit('wallstreetbets')
 
-    for post in subreddit.hot(limit=1000):
+    for post in subreddit.hot(limit=2000):
         tickers_found = []
         post_date = datetime.fromtimestamp(post.created_utc, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
         #print(f"Title: {post.title}")
         title = post.title
         title_split = title.split()
-        titles = []
+        sentiment_score = analyzer.polarity_scores(title)["compound"]
         upvotes = post.score
         for token in title_split:
             if token.startswith('$'):
@@ -44,10 +46,11 @@ def get_posts():
             if token in watchlist:
                 tickers_found.append(token)
                 if token not in ticker_count:
-                    ticker_count[token] = {"Ticker": token, "Title": [title], "Post_Date": post_date, "Mentions": 1, "Upvotes": upvotes, "Likes_per_mention": 0}
+                    ticker_count[token] = {"Ticker": token, "Title": [title], "Post_Date": post_date, "Mentions": 1, "Upvotes": upvotes, "Likes_per_mention": 0, "Sentiment_Analysis": [sentiment_score]}
                 else:
                     ticker_count[token]["Mentions"] += 1
                     ticker_count[token]["Title"].append(title)
+                    ticker_count[token]["Sentiment_Analysis"].append(sentiment_score)
 
     for ticker in ticker_count:
         mentions = ticker_count[ticker]["Mentions"]
