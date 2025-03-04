c3bd23d8c4b377e5d4b20a094c5825eddc983ba2
Jenil Shingala <jenilshingala1002@gmail.com>
2025-03-04 11:11:15 -0800

    Intial Scrapping the data

---
+++ config.py
@@ -0,0 +1,16 @@
+
+
+# Reddit API credentials
+CLIENT_ID = "vMsQfglqWPqZ1NtCCJD2Fw"
+CLIENT_SECRET = "4wrt_K_YthAcWK2-ePQOjJHm7ebqZw"
+
+# Alpha Vantage API key
+API_KEY = "6T9R1LCWHVS88CHT"
+
+# Watchlist of ticker symbols to track
+watchlist = [
+    # All your tickers as shown above
+]
+
+# Dictionary to keep track of ticker counts
+ticker_count = {}
--- historical.py
+++ historical.py
@@ -15,22 +15,15 @@ reddit = praw.Reddit(
 
 pp = pprint.PrettyPrinter(indent=1)
 
-
 def get_historic_data(symbol):
     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}'
-
     r = requests.get(url)
-
     data = r.json()
-
     return data
-
-
 
 def get_posts():
     subreddit = reddit.subreddit('wallstreetbets')
     results = []
-
     for post in subreddit.hot(limit=1000):
         tickers_found = []
         post_date = datetime.fromtimestamp(post.created_utc, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
@@ -38,36 +31,31 @@ def get_posts():
         title = post.title
         title = title.split()
         upvotes = post.score
+        
         for token in title:
             if token.startswith('$'):
                 token = token[1:]
-            if token in watchlist:
-                tickers_found.append(token)
-                if token not in ticker_count:
-                    ticker_count[token] = {"Ticker": token, "Post_Date": post_date, "Mentions": 1, "Upvotes": upvotes, "Likes_per_mention": 0}
-                else:
-                    ticker_count[token]["Mentions"] += 1
-
+                if token in watchlist:
+                    tickers_found.append(token)
+                    if token not in ticker_count:
+                        ticker_count[token] = {"Ticker": token, "Post_Date": post_date, "Mentions": 1, "Upvotes": upvotes, "Likes_per_mention": 0}
+                    else:
+                        ticker_count[token]["Mentions"] += 1
+    
     for ticker in ticker_count:
         mentions = ticker_count[ticker]["Mentions"]
         if ticker_count[ticker]["Upvotes"] > 0:
             ticker_count[ticker]["Likes_per_mention"] = ticker_count[ticker]["Upvotes"]/mentions
-
-        
-    df = pd.DataFrame.from_dict(ticker_count, orient="index") 
+    
+    df = pd.DataFrame.from_dict(ticker_count, orient="index")
     df.to_csv('reddit_data.csv', index=False)
-
-        # print(post_date)
-        
-
-
+    # print(post_date)
 
 def main():
     try:
         get_posts()
     except KeyboardInterrupt:
         print("Exiting gracefully...")
-
 
 if __name__ == '__main__':
     main()
